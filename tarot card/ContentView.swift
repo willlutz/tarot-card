0a2f60f040fe893291510698127bd458c2eea699
will lutz <wlutz@bsd220.org>
2023-12-26 14:12:08 -0600

    final

--- tarot card/ContentView.swift
+++ tarot card/ContentView.swift
@@ -12,61 +12,43 @@ struct ContentView: View {
     @State private var rotation = 0
     var body: some View {
         NavigationView {
+            
             ZStack {
-                Color.red.opacity(0.4).ignoresSafeArea()
+                
                 VStack {
+                    Image("craystalball")
                     Text("TAROT READING")
                         .font(Font.custom( "bold", size: 36))
                         .font(.system(size: 72))
-                    Spacer()
+              
                 
                 
                 HStack {
+        
                     Image("card \(randomValue)")
                         .resizable()
-                        .frame(width: 75, height: 100, alignment: .center)
+                        .frame(width: 300, height: 400, alignment: .center)
                         .padding ()
                         .rotationEffect(.degrees(Double(rotation)))
                         .rotation3DEffect(.degrees(Double(rotation)), axis: (x: 1, y: 1, z: 0))
                         .onTapGesture {
-                            randomValue = Int.random(in: 1...2) // change # to change cards
-                            withAnimation(.default) {
-                                rotation += 360
-                            }
-                            
-                        }
-                    Image("card \(randomValue)")
-                        .resizable()
-                        .frame(width: 75, height: 100, alignment: .center)
-                        .padding ()
-                        .rotationEffect(.degrees(Double(rotation)))
-                        .rotation3DEffect(.degrees(Double(rotation)), axis: (x: 1, y: 1, z: 0))
-                        .onTapGesture {
-                            randomValue = Int.random(in: 1...2) // change # to change cards
-                            withAnimation(.default) {
-                                rotation += 360
-                            }
-                        }
-                    Image("card \(randomValue)")
-                        .resizable()
-                        .frame(width: 75, height: 100, alignment: .center)
-                        .padding ()
-                        .rotationEffect(.degrees(Double(rotation)))
-                        .rotation3DEffect(.degrees(Double(rotation)), axis: (x: 1, y: 1, z: 0))
-                        .onTapGesture {
-                            randomValue = Int.random(in: 1...2) // change # to change cards
+                            randomValue = Int.random(in: 1...4) // change # to change cards
                             withAnimation(.default) {
                                 rotation += 360
                             }
                         }
                     
                 }
-                Spacer()
+        
                 
                 NavigationLink("Card Meaning", destination: Meanings())
-            }}
+            }
+                
+            }
+            
         }
-        Spacer()
+        
+      
         .padding()
         .navigationTitle("Drawing")
         .navigationBarHidden(true)
@@ -80,18 +62,3 @@ struct ContentView_Previews: PreviewProvider {
     }
 }
 
-struct InstructionsView: View {
-    var body: some View {
-        ZStack {
-            Color.gray.opacity(0.7).ignoresSafeArea ()
-            VStack {
-                Image ("").resizable().frame (width: 150, height: 150)
-                Text ("Tarot").font(.title)
-                VStack (alignment: .leading) {
-                    
-                }
-                Spacer()
-            }
-        }
-    }
-}
--- tarot card/Meanings.swift
+++ tarot card/Meanings.swift
@@ -9,12 +9,30 @@ import SwiftUI
 
 struct Meanings: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
-
-struct Meanings_Previews: PreviewProvider {
-    static var previews: some View {
-        Meanings()
+        ZStack{
+            
+            
+            VStack{
+               Text("Card Meanings")
+                    .font(Font.custom( "bold", size: 36))
+                    .font(.system(size: 72))
+                    .padding()
+                Text("01 The Fool")
+    
+                Text("Beginnings,innocence,spontaneity, a free spirit")
+                
+                Text("12 The Hanged Man")
+                Text("Pause, surrender, letting go, new perspectives")
+                Text("13 Death")
+                Text("Endings, change, transformation, transition")
+                Text("18 The Moon")
+                Text("Illusion, fear, anxiety, subconscious, intuition")
+                Spacer()
+            }}
+        }
+    struct Meanings_Previews: PreviewProvider {
+        static var previews: some View {
+            Meanings()
+        }
     }
 }
